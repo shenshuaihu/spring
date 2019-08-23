1ed32179b75321c6e275d63ed75b4f3ecfe470eb
沈帅虎 <shuaihu.shen@hand-china.com>
2019-08-23 17:46:25 +0800

    [ADD] spring-data

--- spring-data-oracle/src/test/java/com/example/demo/SpringDataOracleApplicationTests.java
+++ spring-data-oracle/src/test/java/com/example/demo/SpringDataOracleApplicationTests.java
@@ -1,4 +1,4 @@
-package com.ch5;
+package com.example.demo;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class HandApplicationTests {
+public class SpringDataOracleApplicationTests {
 
     @Test
     public void contextLoads() {
